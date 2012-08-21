2e5da90aa2fe32fda9bb6271123ff7232f04502a
Alex Depps <deppkind@gmail.com>
2012-08-21 16:29:58 +0400

    Renamed to .sh and added git-compleltion

---
+++ aliases.sh
@@ -0,0 +1,18 @@
+# Heroku git deploy
+alias hp='git add .; git commit -m "pushed by hp"; git push heroku master;'
+alias hrm='heroku run rake db:migrate'
+
+# Change fucking mac address
+alias mac='sudo ifconfig en0 ether 001b63af02b2'
+
+# Capistrano and git deploy
+alias gapd='git add .; git commit -m "pushed by gapd"; git push; cap deploy;'
+alias gapdm='git add .; git commit -m "pushed by gapd"; git push; cap deploy:migrations;'
+alias ctl='cap tail_log'
+
+alias hpd='git add .; git commit -m "pusher by heroku"; git push heroku master;'
+
+alias gmapd='git add .; git commit -m "pushed by auto-monkey"; git push gitolite monkey; cap monkey deploy;'
+alias gmapdm='git add .; git commit -m "pushed by auto-monkey"; git push gitolite monkey; cap monkey deploy:migrations;'
+
+function cr() { cap remote "$@" ;}
---
+++ bashrc.sh
@@ -0,0 +1,5 @@
+. ~/bin/dotfiles/colors.sh
+. ~/bin/dotfiles/prompt.sh
+. ~/bin/dotfiles/aliases.sh
+. ~/bin/dotfiles/git-completion.sh
+. ~/bin/dotfiles/config.sh
