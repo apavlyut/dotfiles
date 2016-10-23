7aa4c4a61b85fe86debf5f4e96f32efa9035879d
Alex Pavlyut <alex@pavlyut.com>
2016-10-24 01:07:28 +0300

    Lets go

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,5 @@
 # Heroku git deploy
-alias hp='git add .; git add -u; git commit -am "pushed by heroku"; git push heroku master;'
+alias hp='git add .; git add -u; git commit -am "qick fix and deploy"; git push heroku master;'
 
 alias hrm='heroku run rake db:migrate'
 
